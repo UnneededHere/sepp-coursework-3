7122ea5b20896cae0c7d5839ce76f00794b16191
UnneededHere <krjs@outlook.com>
2023-04-10 15:15:50 +0100

    ListEventReviewsCommand implemented

---
+++ src/main/java/command/ListEventReviewsCommand.java
@@ -0,0 +1,53 @@
+package command;
+
+import java.util.List;
+import java.util.Map;
+
+import controller.Context;
+import model.*;
+import view.IView;
+
+public class ListEventReviewsCommand implements ICommand {
+    private String eventTitle;
+    private List<Review> reviews;
+    
+    public ListEventReviewsCommand(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+    
+    @Override
+    public void execute(Context context, IView view) {
+        // Implementation of command logic to retrieve event reviews and add them to the reviews list
+        // ...
+        List<Event> events = context.getEventState().getAllEvents();
+        for (Event event : events) {
+            if (event.getTitle() == this.eventTitle) {
+                reviews = event.getReviews();
+                view.displaySuccess(
+                    "ListEventReviewsCommand",
+                    LogStatus.LIST_EVENT_REVIEWS_SUCCESS,
+                    Map.of("eventTitle", eventTitle,
+                            "reviews", reviews)
+                );
+                return;
+            }
+        } 
+        view.displayFailure(
+                "ListEventReviewsCommand",
+                LogStatus.LIST_EVENT_REVIEWS_INVALID_EVENT_TITLE,
+                Map.of("eventTitle", eventTitle)
+        );
+        reviews = null;
+        return;
+    }
+
+    @Override
+    public Object getResult() {
+        return reviews;
+    }
+
+    private enum LogStatus {
+        LIST_EVENT_REVIEWS_SUCCESS,
+        LIST_EVENT_REVIEWS_INVALID_EVENT_TITLE
+    }
+}
