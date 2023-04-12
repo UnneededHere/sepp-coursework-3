12badf98356ff6a04d8e854790b3eb3e9a8fd49c
Gregory12321 <s2159025@ed.ac.uk>
2023-04-12 13:48:41 +0100

    AddEventTag

--- src/main/java/model/EventTag.java
+++ src/main/java/model/EventTag.java
@@ -1,5 +1,28 @@
 package model;
 
+
+import java.util.Set;
+
 public class EventTag {
+
+    public Set<String> values;
+    public String defaultValue;
+
+    public EventTag(Set<String> values,
+            String defaultValue){
+        this.values = values;
+        this.defaultValue = defaultValue;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
