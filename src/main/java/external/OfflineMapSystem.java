7af925554ef8be89094d544c186bbb58f29fd357
Gregory12321 <s2159025@ed.ac.uk>
2023-04-13 00:48:50 +0100

    build

--- src/main/java/external/OfflineMapSystem.java
+++ src/main/java/external/OfflineMapSystem.java
@@ -9,9 +9,11 @@ import com.graphhopper.config.LMProfile;
 import com.graphhopper.config.Profile;
 import com.graphhopper.routing.weighting.custom.CustomProfile;
 import com.graphhopper.util.*;
+import com.graphhopper.util.shapes.BBox;
 import com.graphhopper.util.shapes.GHPoint;
+import model.TransportMode;
 
-
+import java.util.Locale;
 
 
 public class OfflineMapSystem implements MapSystem{
@@ -31,10 +33,40 @@ public class OfflineMapSystem implements MapSystem{
         hopper.importOrLoad();
     }
 
+    @Override
+    public GHPoint convertToCoordinates(String coordinatesString) {
+        String[] coordinates = coordinatesString.split(" ");
+        GHPoint ghPoint = new GHPoint(Double.parseDouble(coordinates[0]), Double.parseDouble(coordinates[1]));
+        return ghPoint;
+    }
 
+    @Override
+    public boolean isPointWithinMapBounds(GHPoint coordinates) {
+        double[] coords = {coordinates.lat, coordinates.lon};
+        BBox point = new BBox(coords);
+        return hopper.getBaseGraph().getBounds().contains(point);
+    }
 
+    @Override
+    public ResponsePath routeBetweenPoints(TransportMode modeOfTransport, GHPoint firstCoordinate, GHPoint secondCoordinate) {
+        GHRequest req = new GHRequest(firstCoordinate.lat, firstCoordinate.lon, secondCoordinate.lat, secondCoordinate.lon).
+                        setProfile(modeOfTransport.name()).
+                        setLocale(Locale.US);
+        GHResponse rsp = hopper.route(req);
+        if (rsp.hasErrors())
+            throw new RuntimeException(rsp.getErrors().toString());
+        ResponsePath path = rsp.getBest();
+        return path;
+    }
 
+    @Override
+    public Translation getTranslation() {
+        return hopper.getTranslationMap().getWithFallBack(Locale.UK);
+    }
 
-
-
+    @Override
+    public void close() {
+        // No need to clean up any resources in the mock system. But it may be necessary in a real system,
+        // e.g., we might want to close any open network sockets or files
+    }
 }
