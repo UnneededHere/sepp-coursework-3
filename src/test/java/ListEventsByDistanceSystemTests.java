dfe6db6352bfbe948bd08595c173ab734588a680
Alastair <s2157894@ed.ac.uk>
2023-04-13 07:41:21 +0100

    ListEventsByDistance Tests

--- src/test/java/ListEventsByDistanceSystemTests.java
+++ src/test/java/ListEventsByDistanceSystemTests.java
@@ -1,2 +1,105 @@
+import command.ListEventsMaxDistanceCommand;
+import command.LogoutCommand;
+import command.UpdateConsumerProfileCommand;
+import controller.Controller;
+import model.Event;
+import model.EventTagCollection;
+import model.TransportMode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ListEventsByDistanceSystemTests extends ConsoleTest{
+    private Controller controller;
+    @BeforeAll
+    void setUp(){
+        controller = createController();
+    }
+
+    @Test
+    @DisplayName("Currently logged in user not consumer")
+    void requestDistanceListAsStaff(){
+        createStaff(controller);
+
+        createEvent(controller,5,5);
+
+        startOutputCapture();
+
+        controller.runCommand(new ListEventsMaxDistanceCommand(true,
+                true,
+                null,
+                TransportMode.car,
+                100000,
+                true,
+                true,
+                null));
+
+        stopOutputCaptureAndCompare(
+                "LIST_EVENTS_MAX_DISTANCE_NOT_CONSUMER"
+        );
+    }
+
+    @Test
+    @DisplayName("User has no address in profile")
+    void userNoAddress(){
+
+        createConsumer(controller);
+
+        controller.runCommand(new UpdateConsumerProfileCommand(
+                "123456",
+                "Andy",
+                "Andyluz@hotmail.com",
+                "071231123112",
+                null,
+                "teeheehee",
+                new EventTagCollection()
+        ));
+
+        startOutputCapture();
+
+        Event event = createEvent(controller,5,5);
+
+        controller.runCommand(new ListEventsMaxDistanceCommand(true,
+                true,
+                null,
+                TransportMode.car,
+                100000,
+                true,
+                true,
+                null
+        ));
+
+        stopOutputCaptureAndCompare(
+                "LIST_EVENTS_MAX_DISTANCE_NO_ADDRESS"
+        );
+    }
+
+    @Test
+    @DisplayName("User not logged in")
+    void userLoggedOutRequestsDistanceList(){
+        createConsumer(controller);
+
+        startOutputCapture();
+
+        controller.runCommand(new LogoutCommand());
+
+        controller.runCommand(new ListEventsMaxDistanceCommand(
+                true,
+                true,
+                null,
+                TransportMode.car,
+                100000,
+                true,
+                true,
+                null
+        ));
+
+        stopOutputCaptureAndCompare(
+                "USER_LOGOUT_SUCCESS",
+                "LIST_EVENTS_MAX_DISTANCE_NOT_LOGGED_IN"
+        );
+    }
+
+
+
 }
